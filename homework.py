6cb3715416349a18b8edd737d0c2e9333b35e6d2
Roman <31464179+grand-roman@users.noreply.github.com>
2021-11-24 14:40:05 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -147,12 +147,13 @@ def main(training):
     print(info_message)
 
 
-packages = [
-    ('SWM', [720, 1, 80, 25, 40]),
-    ('RUN', [15000, 1, 75]),
-    ('WLK', [9000, 1, 75, 180]),
-]
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
 
-for workout_type, data in packages:
-    training = read_package(workout_type, data)
-    main(training)
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
